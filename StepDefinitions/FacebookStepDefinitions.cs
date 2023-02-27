be67bebd7011e1b7c7bf0f552e2e448b1ba41ba2
nos_joel <nos_joel@hotmail.com>
2023-02-27 13:24:30 -0400

    Revert "add missing files"
    This reverts commit 0718c64388e2baa92dc80016c7c5bfc7f6ae4fb9.

---
+++ StepDefinitions/FacebookStepDefinitions.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumSpecflow.StepDefinitions
+{
+    [Binding]
+    public class FacebookStepDefinitions
+    {
+
+        Utilities utils;
+
+        //Constructor to create the browser driver and also pass the driver to the Utilities instance.
+        FacebookStepDefinitions(WebDriver webDriver)
+        {
+            utils = new Utilities(webDriver.driver);
+        }
+
+        [When(@"The user enter the username (.*) and password (.*)")]
+        public void WhenTheUserEnterTheUsernameUserAndPasswordPassword(string user, string password)
+        {
+            utils.On(PageObjects.Facebook.loginEmail).Input(user);
+            utils.On(PageObjects.Facebook.loginPassword).Input(password);
+        }
+
+        [When(@"The user clicks the Login button")]
+        public void WhenTheUserClicksTheLoginButton()
+        {
+            utils.On(PageObjects.Facebook.loginButton).Click();
+        }
+
+        [Then(@"The user cannot autheticate and the following message is displayed (.*)")]
+        public void ThenTheUserCannotAutheticateAndTheFollowingMessageIsDisplayedMessage(string message)
+        {
+            utils.WaitFor().VisibilityOfElement(PageObjects.Facebook.loginErrorMsg, 3);
+            utils.On(PageObjects.Facebook.loginErrorMsg).AssertThat("innerText").Contains(message);
+        }
+    }
+}
